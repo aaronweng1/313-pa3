89b05afffbf109b215de7187dd90d56bc8bdb9d1
aaronweng1 <aaron.weng@live.com>
2023-11-21 12:28:19 -0600

    adawhdawkjdaw

--- client.cpp
+++ client.cpp
@@ -110,13 +110,13 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
 
             // Receive the response from the server
             std::cout << "before cread dmsg->person= " << dmsg->person << std::endl;
-            chan->cread(msg_buffer, sizeof(char));
+            chan->cread(msg_buffer, sizeof(double));
             std::cout << "after cread dmsg->person= " << dmsg->person << std::endl;
 
             // Create a pair of p_num and response and push it to the response_buffer
             std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(msg_buffer + sizeof(datamsg)));
             
-            std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
+            //std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
             response_buffer.push((char*)response_pair, sizeof(std::pair<int, double>));
         }
         else if (*msg_type == FILE_MSG) {
