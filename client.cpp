3c3fc9f51f843e229de665f568b167c189739cb7
aaronweng1 <aaron.weng@live.com>
2023-11-21 17:00:20 -0600

    ghvbhjmmbj

--- client.cpp
+++ client.cpp
@@ -120,7 +120,7 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
             std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(read_buffer));
             
             std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
-            response_buffer.push(reinterpret_cast<char*>(response_pair), sizeof(std::pair<int, double>));
+            response_buffer.push((char*)&response_pair, sizeof(std::pair<int, double>));
         }
         else if (*msg_type == FILE_MSG) {
             // Logging added to debug data sent to the server
