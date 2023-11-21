5bcc17765393cee1eb9a83ace917276a016bc525
aaronweng1 <aaron.weng@live.com>
2023-11-21 13:28:51 -0600

    kjerkjggekjeg

--- client.cpp
+++ client.cpp
@@ -112,12 +112,12 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
             char read_buffer[MAX_MESSAGE];
             std::cout << "before cread dmsg->person= " << dmsg->person << " value= " << *(double*)(read_buffer + sizeof(datamsg)) << std::endl;
             chan->cread(read_buffer, MAX_MESSAGE);
-            std::cout << "1after cread dmsg->person= " << dmsg->person << " value= " << *(read_buffer) << std::endl;
+            //std::cout << "1after cread dmsg->person= " << dmsg->person << " value= " << *(read_buffer) << std::endl;
             std::cout << "2after cread dmsg->person= " << dmsg->person << " value= " << *(double*)(read_buffer) << std::endl;
-            std::cout << "3after cread dmsg->person= " << dmsg->person << " value= " << *(double*)(read_buffer + sizeof(double)) << std::endl;
+            //std::cout << "3after cread dmsg->person= " << dmsg->person << " value= " << *(double*)(read_buffer + sizeof(double)) << std::endl;
 
             // Create a pair of p_num and response and push it to the response_buffer
-            std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(msg_buffer + sizeof(datamsg)));
+            std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(*(double*)(read_buffer)));
             
             std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
             response_buffer.push((char*)response_pair, sizeof(std::pair<int, double>));
