1b50ea86c480371b2f0a2fd2e308641756fa1454
aaronweng1 <aaron.weng@live.com>
2023-11-21 12:30:15 -0600

    back to normalhhb

--- client.cpp
+++ client.cpp
@@ -110,11 +110,11 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
 
             // Receive the response from the server
             std::cout << "before cread dmsg->person= " << dmsg->person << std::endl;
-            chan->cread(msg_buffer, sizeof(double));
+            chan->cread(msg_buffer, MAX_MESSAGE);
             std::cout << "after cread dmsg->person= " << dmsg->person << std::endl;
 
             // Create a pair of p_num and response and push it to the response_buffer
-            std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(msg_buffer + sizeof(datamsg)));
+            std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(msg_buffer + sizeof(double)));
             
             //std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
             response_buffer.push((char*)response_pair, sizeof(std::pair<int, double>));
