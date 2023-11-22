c0c5293c5de0bae2cf874331750f305ad978689f
aaronweng1 <aaron.weng@live.com>
2023-11-21 23:25:08 -0600

    oFC2 YOu

--- FIFORequestChannel.cpp
+++ FIFORequestChannel.cpp
@@ -9,20 +9,6 @@ using namespace std;
 
 std::mutex pipeCreationMutex;
 
-int FIFORequestChannel::open_pipe(std::string _pipe_name, int mode) {
-    std::lock_guard<std::mutex> lock(pipeCreationMutex);  // Lock the mutex during pipe creation
-
-    if (mode == CLIENT_SIDE) {
-        mkfifo(_pipe_name.c_str(), 0666);
-        int wfd = open(_pipe_name.c_str(), O_WRONLY);
-        return wfd;
-    } else if (mode == SERVER_SIDE) {
-        int rfd = open(_pipe_name.c_str(), O_RDONLY);
-        return rfd;
-    }
-    return -1;
-}
-
 FIFORequestChannel::~FIFORequestChannel () { 
 	close(wfd);
 	close(rfd);
@@ -35,15 +21,18 @@ FIFORequestChannel::~FIFORequestChannel () {
 /*			MEMBER FUNCTIONS FOR CLASS	R e q u e s t C h a n n e l			*/
 /*--------------------------------------------------------------------------*/
 
-int FIFORequestChannel::open_pipe (string _pipe_name, int mode) {
-    std::lock_guard<std::mutex> lock(pipeMutex);
+int FIFORequestChannel::open_pipe(std::string _pipe_name, int mode) {
+    std::lock_guard<std::mutex> lock(pipeCreationMutex);  // Lock the mutex during pipe creation
 
-	mkfifo (_pipe_name.c_str (), 0600);
-	int fd = open(_pipe_name.c_str(), mode);
-	if (fd < 0){
-		EXITONERROR(_pipe_name);
-	}
-	return fd;
+    if (mode == CLIENT_SIDE) {
+        mkfifo(_pipe_name.c_str(), 0666);
+        int wfd = open(_pipe_name.c_str(), O_WRONLY);
+        return wfd;
+    } else if (mode == SERVER_SIDE) {
+        int rfd = open(_pipe_name.c_str(), O_RDONLY);
+        return rfd;
+    }
+    return -1;
 }
 
 int FIFORequestChannel::cread (void* msgbuf, int msgsize) {
