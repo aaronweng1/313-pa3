12c3430c3bc3997857c671c68d3be2f8f8f69dc6
aaronweng1 <aaron.weng@live.com>
2023-11-21 19:14:57 -0600

    hgcfcghjiokhcfxbhjkl,

--- client.cpp
+++ client.cpp
@@ -101,7 +101,7 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
         MESSAGE_TYPE* msg_type = (MESSAGE_TYPE*)msg_buffer;
 
         if (*msg_type == DATA_MSG) {
-            usleep(rand() % 5000);
+            //usleep(rand() % 5000);
             datamsg* dmsg = (datamsg*)msg_buffer;
             //std::cout << "Sending DATA_MSG to server: person=" << dmsg->person << " time=" << dmsg->seconds << " ecgno=" << dmsg->ecgno << std::endl;
 
@@ -122,7 +122,7 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
             std::pair<int, double>* response_pair = new std::pair<int, double>(dmsg->person, *(double*)(read_buffer));
             
             //std::cout << "Received response: person=" << response_pair->first << " value=" << response_pair->second << std::endl;
-            response_buffer.push((char*)response_pair, sizeof(std::pair<int, double>));
+            response_buffer.push((char*)&response_pair, sizeof(std::pair<int, double>));
         }
         else if (*msg_type == FILE_MSG) {
             // Logging added to debug data sent to the server
