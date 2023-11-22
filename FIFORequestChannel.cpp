a1405db2031f7ec249600a6abe35f713d8ab9c7c
aaronweng1 <aaron.weng@live.com>
2023-11-21 23:40:12 -0600

    kjafhafagafnaewbfaewgfa

--- FIFORequestChannel.cpp
+++ FIFORequestChannel.cpp
@@ -2,7 +2,6 @@
 #include <mutex>
 
 using namespace std;
-std::mutex pipeCreationMutex;  // Declare a global mutex
 
 /*--------------------------------------------------------------------------*/
 /*		CONSTRUCTOR/DESTRUCTOR FOR CLASS	R e q u e s t C h a n n e l		*/
@@ -36,19 +35,15 @@ FIFORequestChannel::~FIFORequestChannel () {
 /*			MEMBER FUNCTIONS FOR CLASS	R e q u e s t C h a n n e l			*/
 /*--------------------------------------------------------------------------*/
 
-int FIFORequestChannel::open_pipe(std::string _pipe_name, int mode) {
-    std::lock_guard<std::mutex> lock(pipeCreationMutex);  // Lock the mutex during pipe creation
-
-    if (mode == CLIENT_SIDE) {
-        mkfifo(_pipe_name.c_str(), 0666);
-        int wfd = open(_pipe_name.c_str(), O_WRONLY);
-        return wfd;
-    } else if (mode == SERVER_SIDE) {
-        int rfd = open(_pipe_name.c_str(), O_RDONLY);
-        return rfd;
-    }
-    return -1;
+int FIFORequestChannel::open_pipe (string _pipe_name, int mode) {
+	mkfifo (_pipe_name.c_str (), 0600);
+	int fd = open(_pipe_name.c_str(), mode);
+	if (fd < 0){
+		EXITONERROR(_pipe_name);
+	}
+	return fd;
 }
+
 int FIFORequestChannel::cread (void* msgbuf, int msgsize) {
 	return read (rfd, msgbuf, msgsize); 
 }
--- client.cpp
+++ client.cpp
@@ -96,7 +96,7 @@ void worker_thread_function(BoundedBuffer& request_buffer, BoundedBuffer& respon
     //      - open the file in update mode
     //      - fseek(SEEK_SET) to offset of the filemsg
     //      - write the buffer from the server
-
+    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
     std::cout << "worker_thread function_running" << std::endl;
     while (true) {
         char msg_buffer[MAX_MESSAGE];
