32499b1897a7ff6955345664ebd705be4e2b1925
aaronweng1 <aaron.weng@live.com>
2023-11-21 23:26:29 -0600

    76163518258996

--- FIFORequestChannel.cpp
+++ FIFORequestChannel.cpp
@@ -2,12 +2,27 @@
 #include <mutex>
 
 using namespace std;
+std::mutex pipeCreationMutex;  // Declare a global mutex
 
 /*--------------------------------------------------------------------------*/
 /*		CONSTRUCTOR/DESTRUCTOR FOR CLASS	R e q u e s t C h a n n e l		*/
 /*--------------------------------------------------------------------------*/
 
-std::mutex pipeCreationMutex;
+FIFORequestChannel::FIFORequestChannel (const string _name, const Side _side) : my_name( _name), my_side(_side) {
+	pipe1 = "fifo_" + my_name + "1";
+	pipe2 = "fifo_" + my_name + "2";
+		
+	if (_side == SERVER_SIDE){
+		wfd = open_pipe(pipe1, O_WRONLY);
+		rfd = open_pipe(pipe2, O_RDONLY);
+	}
+	else{
+		rfd = open_pipe(pipe1, O_RDONLY);
+		wfd = open_pipe(pipe2, O_WRONLY);
+		
+	}
+	
+}
 
 FIFORequestChannel::~FIFORequestChannel () { 
 	close(wfd);
@@ -34,7 +49,6 @@ int FIFORequestChannel::open_pipe(std::string _pipe_name, int mode) {
     }
     return -1;
 }
-
 int FIFORequestChannel::cread (void* msgbuf, int msgsize) {
 	return read (rfd, msgbuf, msgsize); 
 }
